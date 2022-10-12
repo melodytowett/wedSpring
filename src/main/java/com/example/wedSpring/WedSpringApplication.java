5b3c71e004a400190c60c33777496e7b7fbc8398
melodytowett <melodytowett99@gmail.com>
2022-10-12 12:14:35 +0300

    Spring boot frontend application

---
+++ src/main/java/com/example/wedSpring/WedSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.wedSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wedSpring/WedSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wedSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
